89a7710df9c19f5980e6aa532b36f28bbc20d154
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-08-05 17:02:12 -0700

    added pathing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import './App.css';
+import Header from './Header.js';
+
+import Footer from './Footer.js';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <>
+      <Header/>
+     
+      <Footer />
+      </>
     )
   }
 }
