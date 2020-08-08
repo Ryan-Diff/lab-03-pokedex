fd4392f992deb563a0c6a4dc72e5e6503e9ecdd0
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-08-07 18:16:27 -0700

    Working Pagenation

--- src/SearchSection/Params.js
+++ src/SearchSection/Params.js
@@ -6,15 +6,15 @@ export default class Params extends Component {
     render() {
         return (
             <div className="searchDiv">
-                <input onChange={this.props.handleSearchInput} />
+                 <input onChange={this.props.handleSearchInput} value={this.props.search} />
                 <p>Search Pokemon By...</p>
-                <select onChange={this.props.handleSelectChange}>
+                <select onChange={this.props.handleSelectChange} value={this.props.searchBy}>
                     <option value='pokemon'>name</option>
                     <option value='type'>type</option>
                     <option value='attack'>attack</option>
                     <option value='defense'>defense</option>
                 </select>
-                <button onClick={this.props.handleSearchClick}>Search</button>
+                <button onClick={this.props.handleSearchClick}>See Your Pokemon!</button>
             </div>
         )
     }
